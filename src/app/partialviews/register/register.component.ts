e5eceef0519ac7024de2e880ca38d0fca961223a
Jake Kobs <jkobsx4@hotmail.com>
2020-01-21 12:38:16 -0500

    Fixed registration and username. Also tests if username is taken

--- src/app/partialviews/register/register.component.ts
+++ src/app/partialviews/register/register.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { UsersServiceService } from 'src/app/services/users.service';
 import { Users } from '../../models/Users';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  usersarr = [];
+  usersarr: Users[] = [];
+  stopGap :number = 0;
+  allUsers: Observable<Users[]> = this.usersService.getAllUsers();
   username: string;
   password: string;
   email: string;
   fname: string;
   lname: string;
+  
   ccInfo: string; //Dummy field. Not implemented in the backend.
   billingImage: string = "https://images.squarespace-cdn.com/content/v1/5a760a4890bade7aa2cc94b2/1530909622840-T9MKK1Y7MHGJRECSTWXQ/ke17ZwdGBToddI8pDm48kIAfDUahperg14ErRP7t_GMUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcVl31PG4RPDp27sr4LX07pBdzedAFAOAiPqYIuaZjezyMQNkVVzH_xWFOabp85M7O/credit-card-logos+copy.png?format=300w";
   constructor(private usersService: UsersServiceService, private router: Router) { }
@@ -30,12 +34,28 @@ export class RegisterComponent implements OnInit {
     this.createUser();
   }
   createUser(){
-    this.usersService.registerUser(new Users(this.username,this.password,this.email,this.fname,this.lname)).subscribe(
+    this.allUsers.subscribe(
       (response) => {
-        console.log(response);
+        this.usersarr = response;
+        for (let i = 0; i < this.usersarr.length; i++){
+          if(this.usersarr[i].username === this.username){
+            this.stopGap = 1;
+          }
+          
+        }
+        if(this.stopGap === 0){
+          this.usersService.registerUser(new Users(this.username,this.password,this.email,this.fname,this.lname)).subscribe(
+            (response) => {
+              
+            }
+          );
+          this.router.navigate(['/home']);
+        } 
       }
     );
-    this.router.navigate(['/home']);
-  }
-  
+    
+    }
+    resetStopGap(){
+      this.stopGap = 0;
+    }
 }
